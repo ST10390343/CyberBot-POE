74e217a9efe095e25fdd448f466d12e9b4ba15c1
Phodzo <st10390343@myemeris.edu.za>
2026-03-31 22:54:09 +0200

    added Harvard references as comments

--- DefaultResponse.cs
+++ DefaultResponse.cs
@@ -14,3 +14,14 @@ namespace POE
     }
 }
 
+// Reference
+
+// CodeBeauty, 2024. C# String – How to work with strings in C#? C# Programming tutorial for Beginners. [video online] Available at: <https://www.youtube.com/watch?v=nk2zDKK1pvA> [Accessed 31 March 2026].
+
+// Proud, N., 2023. Create Your Own Chat App: SignalR Mastery in C# & ASP.NET. [video online] Available at: <https://www.youtube.com/watch?v=_gJ0NbNKKZg> [Accessed 31 March 2026].
+
+// The Independent Institute of Education (IIE), 2026. Programming 2A [PROG6221/w Module Outline]. [online via internal VLE] The Independent Institute of Education. Available at: <Student Portal> [Accessed 31 March 2026].
+
+// W3Schools, 2026. C# Tutorial. Available at: <https://www.w3schools.com/cs/index.php> [Accessed 31 March 2026].
+
+
